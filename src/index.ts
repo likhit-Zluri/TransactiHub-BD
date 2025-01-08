cb97e816aea7d61a4ea759d80c4081e9c53452cc
likhit-Zluri <likhit.r@zluri.com>
2025-01-08 16:10:38 +0530

    MikroORM added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from "express";
-import pool from "../config/database"; 
+import { initializeORM } from "./config/mikro-orm.config";
+
+import pool from "./config/database"; // direct local DB
 
 // Instantiate express
 const app = express();
@@ -10,17 +12,22 @@ const port = process.env.PORT || 3000;
 // Middleware to parse JSON
 app.use(express.json());
 
+initializeORM();
+
 // Verify database connection
-pool
-	.connect()
-	.then(() => {
-		console.log("Connected to the database successfully");
-	})
-	.catch((err) => {
-		console.error("Error connecting to the database:", err.stack);
-	});
+// direct local DB
+
+// pool
+// 	.connect()
+// 	.then(() => {
+// 		console.log("Connected to the database successfully");
+// 	})
+// 	.catch((err) => {
+// 		console.error("Error connecting to the database:", err.stack);
+// 	});
 
 // Default home route
+
 app.get("/", (req: Request, res: Response) => {
 	res.json({ message: "Welcome to the Server!" });
 });
