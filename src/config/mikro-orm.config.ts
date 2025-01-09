ed4fa2ae3156a0f95193e5da02697b2e44ab970c
likhit-Zluri <likhit.r@zluri.com>
2025-01-09 11:23:39 +0530

    minor changes to mikroorm config

--- src/config/mikro-orm.config.ts
+++ src/config/mikro-orm.config.ts
@@ -7,9 +7,7 @@ import { config } from "dotenv";
 import { Transaction } from "../entities/Transaction";
 config();
 
-console.log({ e: process.env });
-
-const mikroOrmConfig: Options  ={
+const mikroOrmConfig: Options = {
 	driver: PostgreSqlDriver, // Use the correct driver
 	entities: [Transaction], // MikroORM entities
 	dbName: process.env.DB_NAME, // Database name
@@ -17,8 +15,9 @@ const mikroOrmConfig: Options  ={
 	password: process.env.DB_PASSWORD, // Database password
 	host: process.env.DB_HOST, // Database host
 	port: Number(process.env.DB_PORT), // Database port
-	debug: true,
-	
+	// debug: true,
+	debug: process.env.NODE_ENV !== "test", // Disable debug logs during tests
+
 	// extensions: [SeedManager, Migrator],
 };
 export default mikroOrmConfig;
@@ -26,9 +25,11 @@ export default mikroOrmConfig;
 export async function initializeORM() {
 	try {
 		const orm = await MikroORM.init(mikroOrmConfig);
-		// Your logic after initializing the ORM
-		console.log("MikroORM initialized successfully.");
-	} catch (error) {
-		console.error("Error initializing MikroORM:", error);
+		// console.log("MikroORM initialized successfully.");
+
+		return orm;
+	} catch (error: unknown) {
+		// console.error("Error initializing MikroORM:", error);
+		throw new Error(`Error initializing MikroORM: ${error}`);
 	}
 }
