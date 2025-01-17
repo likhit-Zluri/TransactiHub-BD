68956715b8e1e43b8baacf1a013787be965d494d
likhit-Zluri <likhit.r@zluri.com>
2025-01-17 12:38:06 +0530

    handled error in csv upload: multiple files being uploaded and  file being uploaded under invalid file name

--- src/utils/multerConfig.ts
+++ src/utils/multerConfig.ts
@@ -24,61 +24,15 @@ export const upload = multer({
 	},
 });
 
-// Works only when a single file is being uploaded
-// export const handleCSVUpload = (
-// 	req: Request,
-// 	res: Response,
-// 	next: NextFunction
-// ) => {
-// 	upload.single("file")(req, res, (err: any) => {
-// 		console.log("req",req);
-// 		console.log("req.file",req.file)
-// 		console.log("file",typeof req.file,typeof req.files);
-
-// 		// Check if multiple files are uploaded
-// 		if (Array.isArray(req.files) && req.files.length > 1) {
-// 			return res.status(400).json({
-// 				message: "Multiple files are not allowed. Please upload only one file.",
-// 			});
-// 		}
-
-// 		if (err) {
-// 			console.log("error in multertConfig", err);
-
-// 			// Handle file size error
-// 			if (err.code === "LIMIT_FILE_SIZE") {
-// 				return res.status(400).json({
-// 					message: `${err.message}. Only file size less than 1MB is supported`,
-// 				});
-// 			}
-
-// 			// Handle invalid file type error
-// 			if (err.code === "ERROR_FILE_TYPE") {
-// 				return res.status(400).json({ message: err.message });
-// 			}
-
-// 			// Handle other errors
-// 			return res
-// 				.status(500)
-// 				.json({ message: "File upload error.", error: err.message });
-// 		}
-
-// 		next(); // Continue to the next middleware or route handler
-// 	});
-// };
-
-
-
-
 export const handleCSVUpload = (
 	req: Request,
 	res: Response,
 	next: NextFunction
 ) => {
 	upload.single("file")(req, res, (err: any) => {
-		console.log("req",req);
-		console.log("req.file",req.file)
-		console.log("file",typeof req.file,typeof req.files);
+		console.log("req", req);
+		console.log("req.file", req.file, req.files);
+		console.log("file", typeof req.file, typeof req.files);
 
 		// Check if multiple files are uploaded
 		if (Array.isArray(req.files) && req.files.length > 1) {
@@ -102,12 +56,27 @@ export const handleCSVUpload = (
 				return res.status(400).json({ message: err.message });
 			}
 
+			// Handle incorrect field name or more than 1 file being uploaded
+			if (err.code === "LIMIT_UNEXPECTED_FILE") {
+				// Handle more than 1 file being uploaded
+				if (err.field !== "file") {
+					return res.status(400).json({
+						message: `${err.message}. File should be uploaded under the 'file' field.`,
+					});
+				}
+				// // Handle more than 1 file being uploaded under an unexpected field
+				return res
+					.status(400)
+					.json({ message: "Only one file should be uploaded" });
+			}
+
 			// Handle other errors
 			return res
 				.status(500)
 				.json({ message: "File upload error.", error: err.message });
 		}
 
-		next(); // Continue to the next middleware or route handler
+		// Continue to the next route handler
+		next();
 	});
 };
